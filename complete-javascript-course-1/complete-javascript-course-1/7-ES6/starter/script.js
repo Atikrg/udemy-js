1c0b3edae4c26a68bd8e0ccc4410b4c3cd7153f3
atik <atikrangnekar28@gmail.com>
2021-03-09 18:19:52 +0530

    'atik'

---
+++ complete-javascript-course-1/complete-javascript-course-1/7-ES6/starter/script.js
@@ -0,0 +1,23 @@
+/* var name5 = 'jane Smith';
+var age = 24;
+name5 = 'jane miller'
+console.log(name5) */
+
+/* {
+    const a= 1;
+    let b = 2;
+    var c = 3
+}
+
+console.log(c)
+
+(function(){
+    var c = 3;
+}
+
+
+
+)(); */
+
+
+let firstName = 'John'
